9633450c12ac09f2a3b8ecf9588fdc79ae9de186
JminJ <jminju254@gmail.com>
2024-10-23 17:44:37 +0900

    UPDATE: state.py -> add sender key

--- src/state.py
+++ src/state.py
@@ -1,10 +1,12 @@
-import typing as t
 import operator
+import typing as t
+
 from langchain_core.messages import BaseMessage
 
 
 class ArxivMultiAgentState(t.TypedDict):
     messages: t.Annotated[t.Sequence[BaseMessage], operator.add]
     next: str
+    sender: str
     target_paper_path: str
     paper_indexes: t.Dict
