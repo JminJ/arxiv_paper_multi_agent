8c786a4645ac846fa7c7f651c468802e279b4ca6
JminJ <jminju254@gmail.com>
2024-10-23 17:46:55 +0900

    UPDATE: paper_agent_utils.py

--- src/agents/paper_agent/utils/paper_agent_utils.py
+++ src/agents/paper_agent/utils/paper_agent_utils.py
@@ -1,27 +1,30 @@
 import os
 import typing as t
-from typing_extensions import Annotated
+
 import fitz
 from icecream import ic
-
-from langchain_core.documents import Document
-from langchain_core.tools import tool
-from langchain_core.runnables import chain
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.document_loaders import ArxivLoader
+from langchain_core.documents import Document
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import chain
+from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
 from langgraph.prebuilt import InjectedState
+from typing_extensions import Annotated
 
-from src.common.common import CHAT_MODEL, RECENT_PAPER_SEARCH_RSS_FORMAT, PDF_DOWNLOAD_DIR
+from src.common.common import (
+    CHAT_MODEL,
+    PDF_DOWNLOAD_DIR,
+    RECENT_PAPER_SEARCH_RSS_FORMAT,
+)
 from src.common.prompts import (
     EXTRACT_ARXIV_PAPER_ID_PROMPT,
-    MAKE_MARKDOWN_FORMAT_RECENT_PAPER_SUMMARY_PROMPT,
     EXTRACT_RECENT_PAPER_TYPE_PROMPT,
+    MAKE_MARKDOWN_FORMAT_RECENT_PAPER_SUMMARY_PROMPT,
 )
 from src.utils.get_paper_page_indexes import ExtractPaperIndexes
 from src.utils.get_rss_url_values import get_processed_entries_from_rss_url
 from src.utils.paper_pdf_handler import paper_pdf_download, paper_pdf_load
-
 
 # @tool
 # def search_paper_from_arxiv(arxiv_paper_ids:t.List[str])->t.List[Document]:
@@ -42,7 +45,7 @@ from src.utils.paper_pdf_handler import paper_pdf_download, paper_pdf_load
 #     """논문 내 목차 정보를 수집하고 반환합니다.
 
 #     Args:
-#         pdf_object (pymupdf.Document): 논문 목차 추출 대상 논문. 
+#         pdf_object (pymupdf.Document): 논문 목차 추출 대상 논문.
 
 #     Returns:
 #         t.Dict[str, int]: 수집된 목차 dict. 목차명(str), 페이지(int)로 구성.
@@ -94,12 +97,12 @@ from src.utils.paper_pdf_handler import paper_pdf_download, paper_pdf_load
 #             temp_index_content = temp_index_content[:next_index_start_index]
 
 #         each_index_content_dict[temp_index] = temp_index_content
-    
+
 #     return each_index_content_dict
 
 
 @chain
-def arxiv_paper_search(user_input:str)->t.Tuple[t.List[Document], t.Dict[str, int], str]:
+def arxiv_paper_search(user_input: str) -> t.Tuple[t.List[Document], t.Dict[str, int], str]:
     """arxiv paper id로 paper를 검색합니다.
 
     Args:
@@ -112,10 +115,7 @@ def arxiv_paper_search(user_input:str)->t.Tuple[t.List[Document], t.Dict[str, in
 
     # 1. extract arxiv paper id from user_input.
     extract_paper_id_prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", EXTRACT_ARXIV_PAPER_ID_PROMPT[0]),
-            ("user", EXTRACT_ARXIV_PAPER_ID_PROMPT[1])
-        ]
+        [("system", EXTRACT_ARXIV_PAPER_ID_PROMPT[0]), ("user", EXTRACT_ARXIV_PAPER_ID_PROMPT[1])]
     )
 
     extract_paper_id_chain = extract_paper_id_prompt | client
@@ -123,7 +123,10 @@ def arxiv_paper_search(user_input:str)->t.Tuple[t.List[Document], t.Dict[str, in
     ic(paper_id)
 
     # 2. search arxiv paper
-    paper_infos = [ArxivLoader(query=id, doc_content_chars_max=1, load_all_available_meta=True).load()[0] for id in paper_id]
+    paper_infos = [
+        ArxivLoader(query=id, doc_content_chars_max=1, load_all_available_meta=True).load()[0]
+        for id in paper_id
+    ]
     ic(paper_infos)
 
     # 3. download arxiv paper pdf
@@ -143,9 +146,10 @@ def arxiv_paper_search(user_input:str)->t.Tuple[t.List[Document], t.Dict[str, in
 
     return paper_infos, paper_index_dict, paper_save_paths[0]
 
+
 @tool
-def get_recent_papers(user_input:str)->str:
-    """사용자가 원하는 분야의 최신 논문들의 summary를 제공합니다. 특정 논문 검색은 수행하지 못합니다.
+def get_recent_papers(user_input: str) -> str:
+    """arxiv api를 통해 사용자가 원하는 논문 domain의 최신 논문들의 abstract를 제공합니다. 단, 특정 arxiv id기반 논문 검색은 수행할 수 없습니다.
 
     Args:
         user_input (str): 사용자 입력
@@ -154,12 +158,12 @@ def get_recent_papers(user_input:str)->str:
         str: 최신 논문들을 정리한 markdown format text
     """
     client = ChatOpenAI(model=CHAT_MODEL, temperature=0.0)
-    
+
     # 1. 사용자 입력에서 분야를 추출
     extract_paper_type_prompt = ChatPromptTemplate.from_messages(
         [
             ("system", EXTRACT_RECENT_PAPER_TYPE_PROMPT[0]),
-            ("user", EXTRACT_RECENT_PAPER_TYPE_PROMPT[1])
+            ("user", EXTRACT_RECENT_PAPER_TYPE_PROMPT[1]),
         ]
     )
     extract_paper_type_chain = extract_paper_type_prompt | client
@@ -174,17 +178,22 @@ def get_recent_papers(user_input:str)->str:
     markdown_generate_prompt = ChatPromptTemplate.from_messages(
         [
             ("system", MAKE_MARKDOWN_FORMAT_RECENT_PAPER_SUMMARY_PROMPT[0]),
-            ("user", MAKE_MARKDOWN_FORMAT_RECENT_PAPER_SUMMARY_PROMPT[1])
+            ("user", MAKE_MARKDOWN_FORMAT_RECENT_PAPER_SUMMARY_PROMPT[1]),
         ]
     )
     markdown_generate_chain = markdown_generate_prompt | client
-    recent_papers_markdown = markdown_generate_chain.invoke(input={"rss_entries": rss_entries}).content
+    recent_papers_markdown = markdown_generate_chain.invoke(
+        input={"rss_entries": rss_entries}
+    ).content
     ic(recent_papers_markdown)
-    
+
     return recent_papers_markdown
 
+
 @tool
-def get_user_question_part_contents(target_index_name: str, state: Annotated[dict, InjectedState])->t.List[str]:
+def get_user_question_part_contents(
+    target_index_name: str, state: Annotated[dict, InjectedState]
+) -> t.List[str]:
     """find target index from state.paper_indexes, return page content that contain target index part.
 
     Args:
@@ -197,15 +206,19 @@ def get_user_question_part_contents(target_index_name: str, state: Annotated[dic
     paper_indexes = state["paper_indexes"]
     target_index_page_number = paper_indexes.get(target_index_name, None)
     if target_index_page_number is None:
-        pass # return error message 
-    
+        pass  # return error message
+
     # extract index part pages
     target_paper_pdf = paper_pdf_load(state["target_paper_path"])
     if target_index_page_number == 0:
         target_page_numbers = [target_index_page_number, target_index_page_number + 1]
     else:
-        target_page_numbers = [target_index_page_number - 1, target_index_page_number, target_index_page_number + 1]
-    
+        target_page_numbers = [
+            target_index_page_number - 1,
+            target_index_page_number,
+            target_index_page_number + 1,
+        ]
+
     result_paper_pages = []
     for page_number in target_page_numbers:
         page_content = target_paper_pdf.load_page(page_number).get_text("text")
@@ -215,7 +228,9 @@ def get_user_question_part_contents(target_index_name: str, state: Annotated[dic
 
 
 if __name__ == "__main__":
-    paper_information, paper_indexes, paper_save_path = arxiv_paper_search.invoke(input="2402.09353 논문을 찾아줘")
+    paper_information, paper_indexes, paper_save_path = arxiv_paper_search.invoke(
+        input="2402.09353 논문을 찾아줘"
+    )
     print(f"\n\n{paper_information}")
     print(f"\n{paper_indexes}")
     # print(type(result))
