e73637b107a9d8e471c30d2496e6226521efc03e
JminJ <jminju254@gmail.com>
2024-10-27 22:45:13 +0900

    UPDATE: setting nodes, edges

--- src/graph.py
+++ src/graph.py
@@ -4,68 +4,70 @@ import typing as t
 from langgraph.graph import END, StateGraph
 from langgraph.prebuilt import ToolNode
 from langchain_core.messages import HumanMessage
-from langchain_core.utils.function_calling import convert_to_openai_function
-from langchain_openai import ChatOpenAI
 
-from src.common.prompts import PAPER_TEAM_MEMBER_DESC_PROMPT
 from src.state import ArxivMultiAgentState
 from src.agents.agent import AgentCreator
 from src.agents.agent_node import agent_node
-from src.agents.paper_agent.paper_agents import get_recent_papers, get_user_question_part_contents
-
+from src.agents.paper_agent.paper_agents import paper_team_leader_agent, arxiv_paper_search_agent
+from src.agents.paper_agent.utils.paper_agent_utils import (
+    search_paper_by_arxiv_id,
+    paper_index_extract,
+    get_recent_papers,
+    get_user_question_part_contents,
+)
 from dotenv import load_dotenv
 load_dotenv("../.env")
 
 
-
-## 1. agent define
+## 1. supervisor agent define
 agent_creator = AgentCreator()
-
 supervisor_agent = agent_creator.create_supervisor_agent()
-
-paper_team_leader_agent = agent_creator.create_leader_agent(
-    system_prompt="You are Paper Team's leader.",
-    team_member_desc=PAPER_TEAM_MEMBER_DESC_PROMPT,
-    next_roles=["arxiv_paper_searcher"],
-    tools=[get_recent_papers, get_user_question_part_contents],
-)
-arxiv_paper_search_agent = agent_creator.create_chat_agent(
-    system_prompt="You are Paper Team's member agent, 'arxiv_paper_search_agent'. You will do below jobs:\n1. search paper by arxiv paper id.n\2. download that paper\n3. extract paper's indexes.",
-    next_roles=["paper_team_leader", "supervisor"]
-)
 
 ## 2. agent node define
 supervisor_agent_node = functools.partial(agent_node, agent=supervisor_agent, name="supervisor")
 paper_team_leader_agent_node = functools.partial(agent_node, agent=paper_team_leader_agent, name="paper_team_leader")
+arxiv_paper_search_agent_node = functools.partial(agent_node, agent=arxiv_paper_search_agent, name="arxiv_paper_searcher")
+tool_node = ToolNode(tools=[search_paper_by_arxiv_id, paper_index_extract, get_recent_papers, get_user_question_part_contents]) # 추후 search tools도 적재
 
+## 3. define router function
+def router(state):
+    # This is the router
+    messages = state["messages"]
+    last_message = messages[-1]
+    if last_message.tool_calls:
+        return "call_tool"
+    if "FINAL ANSWER" in last_message.content:
+        return END
+    return state["next_role"]
 
 ## 3. generate graph
-MEMBERS = [
-    "paper_team_leader",
-    # "search_team_leader",
-    # "call_tool"
-]
 workflow = StateGraph(ArxivMultiAgentState)
 workflow.add_node("supervisor", supervisor_agent_node)
 workflow.add_node("paper_team_leader", paper_team_leader_agent_node)
-workflow.add_node("paper_arxiv_paper_searcher", )
-# workflow.add_node("call_tool", tool_node)
+workflow.add_node("arxiv_paper_searcher", arxiv_paper_search_agent_node)
+workflow.add_node("call_tool", tool_node)
 
-for member in MEMBERS:
-    # We want our workers to ALWAYS "report back" to the supervisor when done
-    workflow.add_edge(member, "supervisor")
-workflow.add_conditional_edges("paper_team_leader", lambda x: x["next"], {"arxiv_paper_searcher": "paper_arxiv_paper_searcher"})
-workflow.add_edge("paper_arxiv_paper_searcher", "paper_team_leader")
-
-conditional_map = {k: k for k in MEMBERS}
-conditional_map["FINISH"] = END
-print(f"conditional_map: {conditional_map}")
-workflow.add_conditional_edges("supervisor", lambda x: x["next"], conditional_map)
-
+workflow.add_conditional_edges(
+    "supervisor",
+    router,
+    {"paper_team_leader": "paper_team_leader", END: END}
+)
+workflow.add_conditional_edges(
+    "paper_team_leader", 
+    router, 
+    {"arxiv_paper_searcher": "arxiv_paper_searcher", "call_tool": "call_tool"}
+)
+workflow.add_conditional_edges(
+    "arxiv_paper_searcher",
+    router,
+    {"call_tool": "call_tool"}
+)
+workflow.add_edge("paper_team_leader", "supervisor")
+workflow.add_edge("arxiv_paper_searcher", "paper_team_leader")
 
 workflow.set_entry_point("supervisor")
-print(workflow._all_edges)
 graph = workflow.compile()
+
 
 if __name__ == "__main__":
     from icecream import ic
