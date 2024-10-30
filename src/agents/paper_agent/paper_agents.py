0d38ba37119d709c8929c98d6cdc01a87e0594c3
JminJ <jminju254@gmail.com>
2024-10-30 18:14:49 +0900

    UPDATE: get_recent_upload_papers tool name change apply

--- src/agents/paper_agent/paper_agents.py
+++ src/agents/paper_agent/paper_agents.py
@@ -10,10 +10,10 @@ from langchain_openai import ChatOpenAI
 
 from src.agents.agent import AgentCreator
 from src.agents.paper_agent.utils.paper_agent_utils import (
-    get_recent_papers,
+    get_recent_upload_papers,
     get_user_question_part_contents,
+    paper_index_extract,
     search_paper_by_arxiv_id,
-    paper_index_extract
 )
 from src.common.common import (
     CHAT_MODEL,
@@ -32,11 +32,12 @@ agent_creator = AgentCreator()
 paper_team_leader_agent = agent_creator.create_leader_agent(
     system_prompt="You are Paper Team's leader.",
     team_member_desc=PAPER_TEAM_MEMBER_DESC_PROMPT,
-    next_roles=["arxiv_paper_searcher"],
-    tools=[get_recent_papers, get_user_question_part_contents],
+    next_roles=["arxiv_paper_searcher", "supervisor"],
+    # tools=[get_recent_papers, get_user_question_part_contents],
+    tools=[get_recent_upload_papers, get_user_question_part_contents],
 )
 arxiv_paper_search_agent = agent_creator.create_chat_agent(
     tools=[search_paper_by_arxiv_id, paper_index_extract],
-    system_prompt="You are Paper Team's member agent, 'arxiv_paper_search_agent'. You will do below jobs by your tools:\n1. search paper by arxiv paper id.n\2. download that paper\n3. extract paper's indexes.",
-    next_roles=["paper_team_leader", "supervisor"]
+    system_prompt="You are Paper Team's member agent, 'arxiv_paper_search_agent'. You will do below jobs by your tools:\n- search paper by arxiv paper id.\n- download paper pdf\n- extract paper's indexes.",
+    next_roles=["paper_team_leader"],
 )
